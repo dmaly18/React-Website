0f9cb3b37dd531b0d5f626b6cc7c062249f28b87
Derek Maly <dmaly18@gmail.com>
2023-12-23 18:48:50 -0800

    deleted button

--- src/components/intro.js
+++ src/components/intro.js
@@ -10,7 +10,6 @@ const Intro = () => {
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm <span className="introName">Derek</span> <br />Web developer</span>
                 <p className="introPara">I am a full stack React web developer with experience in creating<br />visually appealing and user friendly websites.</p>
-                <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg'/> Hire Me</button></Link>
             </div>
             <img src={bg} alt="Profile" className="bg" />
         </section>
